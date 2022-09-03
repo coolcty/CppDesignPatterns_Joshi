a9a05e87a7d4830a7bc26818d768c77b3862242e
coolcty <coolcty@qq.com>
2022-09-03 12:06:35 +0200

    update

--- CppDesignPatterns_Joshi/CppDesignPatterns_Joshi.cpp
+++ CppDesignPatterns_Joshi/CppDesignPatterns_Joshi.cpp
@@ -3,10 +3,10 @@
 
 #include <iostream>
 
-int main()
+/*int main()
 {
     std::cout << "Hello World!\n";
-}
+}*/
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
